a4f0f9a3bd50cba9db363e3b4b6e8b17019e34e9
Pavel Bobkov <83582393+tniur@users.noreply.github.com>
2026-01-25 20:52:50 +0300

    init project

---
+++ PQDrop/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  PQDrop
+//
+//  Created by Pavel Bobkov on 25.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View { }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PQDrop/PQDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  PQDropApp.swift
+//  PQDrop
+//
+//  Created by Pavel Bobkov on 25.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct PQDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
