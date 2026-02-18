a5d6ebf7bfb7f3fede0d88c5b46bfabc698de513
ZhuravlevaAnastasia <nas20040908@gmail.com>
2026-02-19 00:52:16 +0300

    fix: fix paddings

--- PQDrop/Onboarding/Views/OnboardingPage.swift
+++ PQDrop/Onboarding/Views/OnboardingPage.swift
@@ -12,25 +12,26 @@ struct OnboardingPage: View {
     private let step: OnboardingStep
     
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(spacing: .zero) {
             step.image
                 .frame(maxWidth: .infinity)
                 .frame(height: 300)
                 .anchorPreference(key: IconAnchorKey.self, value: .bounds) {
                     [step.id: $0]
                 }
-            
-            Color.clear.frame(height: 20)
-            
+                .padding(.bottom, 56)
+                        
             Text(step.title)
                 .font(PQFont.M24)
                 .foregroundStyle(PQColor.base0.swiftUIColor)
                 .multilineTextAlignment(.center)
+                .padding(.bottom, 20)
             
             Text(step.subtitle)
                 .font(PQFont.R14)
                 .foregroundStyle(PQColor.blue2.swiftUIColor)
                 .multilineTextAlignment(.center)
+                .padding(.bottom, 20)
 
             if let markText = step.mark {
                 Text(markText)
