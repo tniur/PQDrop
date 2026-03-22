18734db8de2df62bddc5e591a6b43518d8be706e
ZhuravlevaAnastasia <nas20040908@gmail.com>
2026-03-22 20:28:05 +0300

    feat: add VerificationBadge

---
+++ PQUIComponents/Sources/PQUIComponents/VerificationBadge.swift
@@ -0,0 +1,52 @@
+//
+//  VerificationBadge.swift
+//  PQUIComponents
+//
+//  Created by Анастасия Журавлева on 22.03.2026.
+//
+
+import SwiftUI
+
+public struct VerificationBadge: View {
+
+    // MARK: - Properties
+
+    private let isVerified: Bool
+
+    private var markIcon: Image {
+        isVerified ? PQImage.done.swiftUIImage : PQImage.xmark.swiftUIImage
+    }
+
+    private var markText: String {
+        isVerified ? "Verified" : "Unverified"
+    }
+
+    private var markBackgroundColor: Color {
+        isVerified ? PQColor.green5.swiftUIColor : PQColor.base4.swiftUIColor
+    }
+
+    // MARK: - Body
+
+    public var body: some View {
+        HStack(spacing: 2) {
+            markIcon
+                .renderingMode(.template)
+                .foregroundStyle(PQColor.base0.swiftUIColor)
+
+            Text(markText)
+                .font(PQFont.B12)
+                .foregroundStyle(PQColor.base0.swiftUIColor)
+        }
+        .padding(8)
+        .background(
+            Capsule()
+                .foregroundStyle(markBackgroundColor)
+        )
+    }
+
+    // MARK: - Initializer
+
+    public init(isVerified: Bool) {
+        self.isVerified = isVerified
+    }
+}
